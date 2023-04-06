530fc07ae4d7bcc514f60f3812baaf461d593e1e
Surya <surya.pratap0038@gmail.com>
2023-04-07 00:57:26 +0530

    7th april temp change

--- test.cpp
+++ test.cpp
@@ -1,44 +1,25 @@
-#include<bits/stdc++.h>
+/*Code by rubb3rducky*/
+#include <bits/stdc++.h>
+//#include <boost/multiprecision/cpp_int.hpp>
+#define lli long long
+#define pb push_back
+#define eb emplace_back
+#define pi 3.14159265358979323846
+#define MOD 1000000007
+#define unbuffer cin.clear(); cin.sync();
+#define foi(n)  for(lli i=0;i<n;i++)
+#define foj(n)  for(lli j=0;j<n;j++)
+#define test(T) lli T;cin>>T;while(T--)
+#define loop(i, a, b) for(int i = (a); i<= (b); i++)
 using namespace std;
-void swap(int a[],int i,int j)
-{
-    int t=a[i];
-    a[i]=a[j];
-    a[j]=t;
-}
-int partition(int a[],int l,int h)
-{
-    int p=a[h];//last element
-    int i=l-1;
-    for(int j=l;j<h;j++)
-    {
-        if(a[j]<p)
-        {
-            i++;
-            swap(a,i,j);
-        }
-        swap(a,i+1,h);
-        return i+1;
-    }
-}
-void sort(int a[],int l,int h)
-{
-    if(l<h){
-    int p=partition(a,l,h);
-    sort(a,l,p-1);
-    sort(a,p+1,h);
-    }
-}
+//using namespace boost::multiprecision;
 int main()
 {
-    int n;
-    cin>>n;
-    int l=0,h=n-1;
-    int a[n];
-    for(int i=0;i<n;i++)
-    cin>>a[i];
-    sort(a,l,h);
-    for(int i=0;i<n;i++)
-    cout<<a[i]<<" ";
-    return 0;
+  ios_base::sync_with_stdio(false);
+   cin.tie(NULL);   
+   cout.tie(NULL);
+   test(T)
+   {
+        cout<<"Hello my World!\n";
+   }
 }
